91b365945afada5883512ab02849b672bf198231
wwwal2 <smalex9@gmail.com>
2016-11-24 18:34:00 +0200

    testing

--- testing.js
+++ testing.js
@@ -1,1 +1,11 @@
+//method creating
+function MethodCreate() {
+	this.saySay = function(a) {
+		alert(a);
+	}
+	this.staticAlert = alert("hi");
+}
 
+var doIt = new MethodCreate();
+doIt.saySay("Hello");
+doIt.staticAlert;
